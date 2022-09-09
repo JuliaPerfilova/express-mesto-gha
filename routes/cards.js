01d3077be4317fcd1d5a7182ca993c7e72924692
Julia Perfilova <perfilovajulia@yandex.ru>
2022-09-10 00:10:32 +0300

    fix: hotfix after tests1

--- routes/cards.js
+++ routes/cards.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { celebrate, Joi } = require('celebrate');
 
 const router = express.Router();
 const {
@@ -10,15 +9,7 @@ router.get('/', getCards);
 router.post('/', createCard);
 router.delete('/:cardId', deleteCard);
 
-router.put('/:cardId/likes', celebrate({
-  params: Joi.object().keys({
-    cardId: Joi.string().hex().length(24),
-  }),
-}), likeCard);
-router.delete('/:cardId/likes', celebrate({
-  params: Joi.object().keys({
-    cardId: Joi.string().hex().length(24),
-  }),
-}), dislikeCard);
+router.put('/:cardId/likes', likeCard);
+router.delete('/:cardId/likes', dislikeCard);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { celebrate, Joi } = require('celebrate');
 
 const router = express.Router();
 const {
@@ -8,17 +7,8 @@ const {
 
 router.get('/', getUsers);
 router.get('/me', getCurrentUser);
-router.get('/:id', celebrate({
-  params: Joi.object().keys({
-    id: Joi.string().hex().length(24),
-  }),
-}), getUserbyID);
-router.patch('/me', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
-  }),
-}), updateProfile);
+router.get('/:userId', getUserbyID);
+router.patch('/me', updateProfile);
 router.patch('/me/avatar', updateAvatar);
 
 module.exports = router;
