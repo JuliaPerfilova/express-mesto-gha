6a6b3779ae04ad83bb7ad241b261e2515242435a
Julia Perfilova <perfilovajulia@yandex.ru>
2022-09-11 01:20:16 +0300

    fix: hotfix after tes5

--- routes/users.js
+++ routes/users.js
@@ -10,20 +10,13 @@ const {
 
 router.get('/', getUsers);
 router.get('/me', getCurrentUser);
-
-router.get('/:userId', celebrate({
-  params: Joi.object().keys({
-    id: Joi.string().hex(),
-  }),
-}), getUserbyID);
-
+router.get('/:userId', getUserbyID);
 router.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
   }),
 }), updateProfile);
-
 router.patch('/me/avatar', celebrate({
   avatar: Joi.string().min(2).custom(urlValidation),
 }), updateAvatar);
