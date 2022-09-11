351f4312d109d32e5aa12d41bae523a0a0b9ec50
Julia Perfilova <perfilovajulia@yandex.ru>
2022-09-11 18:53:08 +0300

    fix: hotfix after test7

--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,14 @@ const {
 } = require('../controllers/users');
 
 router.get('/', getUsers);
+
 router.get('/me', getCurrentUser);
-router.get('/:userId', getUserbyID);
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().hex().length(24),
+  }),
+}), getUserbyID);
+
 router.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
